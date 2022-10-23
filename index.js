6896ea37b2a5b598640cdba41c006b005dc514f4
Justin Williams <williams.jman@gmail.com>
2022-10-23 19:52:19 -0400

    Used queryselector to pull the value from the input box on the html page

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const addValues = (event) => {};
+const addValues = (event) => {
+  let total = 0;
+  const value1 = document.querySelector("#Value1").value;
+  const value2 = document.querySelector("#Value2").value;
+};
