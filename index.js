d54dab29a45c917a8aea2d764fc06173e1b4ec08
Justin Williams <williams.jman@gmail.com>
2022-10-23 21:13:34 -0400

    Checks to see if any of the inputs are 0 and if they are not divides the values

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ const subtractValues = (event) => {
 document.querySelector("#subtract").addEventListener("click", subtractValues);
 
 const divideValues = (event) => {
+  let total = 0;
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
+  if (value1 == 0 || value2 == 0) {
+    answer.setAttribute("value", "Error: Cannot divide by 0");
+  } else {
+    total = value1 / value2;
+    //https://www.w3schools.com/jsref/dom_obj_text.asp
+    answer.setAttribute("value", total);
+  }
 };
