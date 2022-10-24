854fbd9ad3b39d737da8d958e4bbdefd1ea00393
Justin Williams <williams.jman@gmail.com>
2022-10-23 20:42:20 -0400

    Put the answer inside of the answer input box so the user can see the total

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const subtractValues = (event) => {
   let total = 0;
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
-  const answer = document.querySelector("#answer");
   total = value1 - value2;
+
+  const answer = document.querySelector("#answer");
+  //https://www.w3schools.com/jsref/dom_obj_text.asp
+  answer.setAttribute("value", total);
 };
