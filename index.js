c7c31dbfe5c300ab0e31ed0410ed71a7a6f43ec0
Justin Williams <williams.jman@gmail.com>
2022-10-23 20:13:30 -0400

    Added total to add the values together

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const addValues = (event) => {
   let total = 0;
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
+  total = +value1 + +value2;
 };
