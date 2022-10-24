6f8f1223f6d3bbcccbcce367a2e332570608d28b
Justin Williams <williams.jman@gmail.com>
2022-10-23 22:24:37 -0400

    Added a event listener to allow the user to click on the button to perfrom the operation

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ const multiplyValues = (event) => {
   let total = 0;
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
-
+  const answer = document.querySelector("#answer");
   total = value1 * value2;
+  //https://www.w3schools.com/jsref/dom_obj_text.asp
+  answer.setAttribute("value", total);
 };
+document.querySelector("#multiply").addEventListener("click", multiplyValues);
