548ecb89382f5c876cefb8510c8221977b68d8ce
Justin Williams <williams.jman@gmail.com>
2022-10-23 20:41:26 -0400

    Created the variable to store the values after we subtract them

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const addValues = (event) => {
 document.querySelector("#add").addEventListener("click", addValues);
 
 const subtractValues = (event) => {
+  let total = 0;
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
+  const answer = document.querySelector("#answer");
+  total = value1 - value2;
 };
