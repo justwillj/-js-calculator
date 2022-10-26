a8040cc2ec40d04cbc7dc9b67f940f9e0496189b
Tiana Langer <langert@midlandstech.edu>
2022-10-26 14:32:45 -0400

    code review

--- index.js
+++ index.js
@@ -5,16 +5,21 @@
  */
 const addValues = (event) => {
   let total = 0;
+  /*CODE REVIEW: since you are getting the values of the inputs in every function we should move this functionality into its own function so we can call it whenever we need to get the values */
   const value1 = document.querySelector("#Value1").value;
   const value2 = document.querySelector("#Value2").value;
+  /*CODE REVIEW: anything to do with displaying the 'answer' can be moved into its own function and we can call it anytime we need to display an answer */
   const answer = document.querySelector("#answer");
   if (value1 === "" || value2 === "") {
+    /*CODE REVIEW: intersting choice using an input to display your 'answers', usually we want to not use an input for showing values, we want to stick to uneditable fields like a paragraph or div */ 
     answer.setAttribute("value", "Error: Please enter a number!");
   } else {
+    /*CODE REVIEW: nice job with the typecasting here, but instead of using a unary operator (+) we want to use the Number() typecast */
     total = +value1 + +value2;
     //https://www.w3schools.com/jsref/dom_obj_text.asp
     answer.setAttribute("value", total);
   }
+  /*CODE REVIEW: why is this memoryOfValues function show up in your code 6 times? We don't redeclare functions multiple times, we declare a function and that's it. */ 
   /**
    * stores the last answer so the user can recall it
    * @param {click} event - when the user clicks on the button, runs the function
@@ -26,6 +31,7 @@ const addValues = (event) => {
   };
   document.querySelector("#memory").addEventListener("click", memoryOfValues);
 };
+/*CODE REVIEW: remember we want to group our addEventListeners into one space so that we can easily find them and not have to dig thorugh all our code when debugging */
 document.querySelector("#add").addEventListener("click", addValues);
 
 /**
